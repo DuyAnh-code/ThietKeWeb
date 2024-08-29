9c7195cda7ea9dbd108b9c1a35cf8c0da13c7907
Le Binh Duy Anh <renbi1204@gmail.com>
2024-08-29 10:20:08 +0700

    Add files via upload

---
+++ Web-CVtest/script.js
@@ -0,0 +1,7 @@
+// script.js
+
+// Ví dụ: Thay đổi màu nền khi nhấp vào nút
+document.addEventListener('DOMContentLoaded', () => {
+    // Thực hiện các tương tác hoặc hiệu ứng nếu cần
+    console.log('CV đã sẵn sàng!');
+});
