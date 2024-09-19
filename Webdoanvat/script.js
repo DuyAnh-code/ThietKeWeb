8fa29482f63fdafb3c1c51794dbe3c5eae73482d
Le Binh Duy Anh <renbi1204@gmail.com>
2024-09-20 00:20:10 +0700

    Delete Webdoanvat/script.js

--- Webdoanvat/script.js
+++
@@ -1,8 +0,0 @@
-// Đây là nơi bạn có thể thêm các chức năng JavaScript cho trang web của bạn
-// Ví dụ: xử lý sự kiện nhấp chuột, tương tác với API, v.v.
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Trang web đã sẵn sàng!');
-    
-    // Bạn có thể thêm mã JavaScript của mình ở đây
-});
